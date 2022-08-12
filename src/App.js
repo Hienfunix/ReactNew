114303dff2974023baf816e983497555e5c024af
HienFX14975 <hientnfx14975@funix.edu.vn>
2022-08-12 15:35:14 +0700

    lab4.1-lab4.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
 import './App.css';
 import Expenses from './Components/Expenses/Expenses'
 import NewExpense from './Components/NewExpenses/NewExpense';
-const App = () => {
-  const expenses = [
+
+  const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -29,14 +30,45 @@ const App = () => {
       date: new Date(2021, 5, 12),
     }  
   ]
-  const addExpenseHandler = expense => {
-    console.log('Add');
-    console.log(expense);
-  }
-  return (
+  
+  
+  // const expenses = [
+  //   {
+  //     id: 'e1',
+  //     title: 'Toilet Paper',
+  //     amount: 94.12,
+  //     date: new Date(2020, 7, 14),
+  //   },
+  //   {
+  //     id: 'e2',
+  //     title: 'New TV',
+  //     amount: 799.49,
+  //     date: new Date(2021, 2, 12)
+  //   },
+  //   {
+  //     id: 'e3',
+  //     title: 'Car Insurance',
+  //     amount: 294.67,
+  //     date: new Date(2021, 2, 28),
+  //   },
+  //   {
+  //     id: 'e4',
+    
+  //   title: 'New Desk (Wooden)',
+  //     amount: 450,
+  //     date: new Date(2021, 5, 12),
+  //   }  
+  // ]
+  const App = () => {
+  const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })}
+
+    return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <h2>Let's get started!</h2>
       <Expenses items={expenses} />
     </div>
   )
--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,24 +1,26 @@
-import React, { useState} from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    const clickHandler = () => {
-        setTitle('Click roi nha!');
-        console.log({title});
-    }
+    // const [title, setTitle] = useState(props.title);
+    // const clickHandler = () => {
+    //     setTitle('Click roi nha!');
+    //     console.log({title});
+    // }
     return (
+        <li>
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
-            <div className='expense-item_description'>
-                <h2>{title}</h2>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>'
             </div>
-            <button onClick={clickHandler}> change here </button>
+            {/* <button onClick={clickHandler}> change here </button> */}
         </Card>
+        </li>
     )
 }
 export default ExpenseItem;
