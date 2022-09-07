94339db337027ab8a3d8c72497e779efd062d2ce
HienFX14975 <hientnfx14975@funix.edu.vn>
2022-09-07 07:15:31 +0700

    add lab9.1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+
+function App() {
+  
+  return (
+    <React.Fragment>
+      <h1> Hello lab 9.1 </h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
