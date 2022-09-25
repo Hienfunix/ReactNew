7812c0dd9fc6e2b1c9280f980a5873a0d119e715
HienFX14975 <hientnfx14975@funix.edu.vn>
2022-09-25 13:14:43 +0700

    lam bt 178

--- src/App.js
+++ src/App.js
@@ -6,23 +6,19 @@ import './App.css';
 function App() {
     const [movie,setMovie] = useState([])
 
-    function fetchMovieHandler (){
-      fetch('https://swapi.py4e.com/api/films/') 
-      .then((response) => { 
-        return response.json();
-      })
-      .then ((data) => {
-       const transformedMovies = data.results.map(movieData => {
+    async function fetchMovieHandler (){
+      const response = await fetch('https://swapi.py4e.com/api/films/');
+      const data = await response.json();
+      const transformedMovies = data.results.map(movieData => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
           releaseDate: movieData.release_date
-        }
+        };
        })
        setMovie(transformedMovies)
-      })
-    }
+      }
 
   return (
     <React.Fragment>
