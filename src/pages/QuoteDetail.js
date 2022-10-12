3962cedabb64fb0b23b5ac731f0613040db32961
HienFX14975 <hientnfx14975@funix.edu.vn>
2022-10-12 18:19:04 +0700

    283 - Adding a Not Found Page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,21 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from '../components/comments/Comments'
+import HighLightedQuote from '../components/quotes/HighlightedQuote'
 const QuoteDetail = () => {
     const params = useParams();
     const prQuote = params.quoteId;
+    const DUMMY_QUOTE = [
+        { id:'q1', author:'Max', text: 'Learning React is fun'},
+        { id:'q2', author:'Maximilian', text: 'Learning React is great'}
+    ]
+    const quote = DUMMY_QUOTE.find(value => value.id === prQuote)
+    if(!quote){
+        return <h1> Quote not found</h1>
+    }
     return (
         <Fragment>
-            <h1>Quote Detail Page</h1>
-            <p>{prQuote}</p>
+            <HighLightedQuote text={quote.text} author={quote.author}/>
             <Route path={`/quotes/${prQuote}/comments`}>
                 <Comments />
             </Route>
